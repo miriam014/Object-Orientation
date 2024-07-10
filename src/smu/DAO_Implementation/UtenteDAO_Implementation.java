e3c7fbd625dd6601edaeb8fb96ba23cb6ca35467
Giulia Gargiulo <giulia.gargiulo3@studenti.unina.it>
2024-07-10 14:00:07 +0200

    aggiunto cose

--- src/smu/DAO_Implementation/UtenteDAO_Implementation.java
+++ src/smu/DAO_Implementation/UtenteDAO_Implementation.java
@@ -11,7 +11,17 @@ import smu.Database;
 public class UtenteDAO_Implementation  implements UtenteDAO {
 
     @Override
-    boolean insert(Utente user) throws SQLException{
+    public Utente getByUsername(String username) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Utente checkLogin(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean insert(Utente user) throws SQLException{
         Connection connection = Database.getConnection();
 
         String sql = "INSERT INTO smu.Utente(Username, Nome, Cognome, Telefono, Email, Password, IdFamiglia) VALUES (?,?,?,?,?,?,?)";
@@ -24,10 +34,20 @@ public class UtenteDAO_Implementation  implements UtenteDAO {
         p.setString(5, user.getEmail());
         p.setString(6, user.getPassword());
         p.setInt(7, user.getIdFamiglia());
-        
+
         int result = p.executeUpdate();
         p.close();
 
-        return result; // Devo ritornare true o false!!! da cambiare
+        return result != 0;
+    }
+
+    @Override
+    public boolean update(Utente user) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Utente user) throws SQLException {
+        return false;
     }
 }
