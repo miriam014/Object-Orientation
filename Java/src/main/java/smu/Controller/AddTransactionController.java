eff0e4afc4c15ccf76e16d10398bdf84f9dff7d6
Tatuuz <for.durso@studenti.unina.it>
2025-01-20 14:40:04 +0100

    Continui famiglia

--- Java/src/main/java/smu/Controller/AddTransactionController.java
+++ Java/src/main/java/smu/Controller/AddTransactionController.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import smu.DAO_Implementation.CategoriaDAOimp;
 import smu.DAO_Implementation.TransazioneDAOimp;
 import smu.DTO.Carta;
@@ -17,26 +15,25 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class AddTransactionController extends HomepageController{
-    @FXML ComboBox<String> TipoTransazione;
-    @FXML TextField NuovoImporto;
-    @FXML ComboBox<String> Valuta;
-    @FXML DatePicker NuovaData;
-    @FXML ComboBox<String> NuovaCategoria;
-    @FXML TextField NuovaCausale;
-    @FXML TextField NuovoDaA;
-    @FXML ComboBox<String> NumeroCarta;
+    @FXML ComboBox<String> tipoTransazione;
+    @FXML TextField nuovoImporto;
+    @FXML ComboBox<String> valuta;
+    @FXML DatePicker nuovaData;
+    @FXML ComboBox<String> nuovaCategoria;
+    @FXML TextField nuovaCausale;
+    @FXML TextField nuovoDaA;
+    @FXML ComboBox<String> numeroCarta;
 
     @FXML
     public void initialize() {
 
-
-        TipoTransazione.setItems(FXCollections.observableArrayList("Entrata", "Uscita"));
-        Valuta.setItems(FXCollections.observableArrayList("EUR", "USD"));
+        tipoTransazione.setItems(FXCollections.observableArrayList("Entrata", "Uscita"));
+        valuta.setItems(FXCollections.observableArrayList("EUR", "USD"));
 
         try {
             CategoriaDAOimp categoriaDAO = new CategoriaDAOimp();
             List<String> categorie = categoriaDAO.getAllCategorie();
-            NuovaCategoria.setItems(FXCollections.observableArrayList(categorie));
+            nuovaCategoria.setItems(FXCollections.observableArrayList(categorie));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -46,7 +43,7 @@ public class AddTransactionController extends HomepageController{
         for (Carta carta : carteUtente) {
             numeriCarte.add(carta.getNumeroCarta());
         }
-        NumeroCarta.setItems(numeriCarte);
+        numeroCarta.setItems(numeriCarte);
     }
 
     @FXML
@@ -59,37 +56,37 @@ public class AddTransactionController extends HomepageController{
             return; // Blocca l'esecuzione in caso di errori
         }
 
-        if("Uscita".equals((String) TipoTransazione.getValue())){
-            destinatario = NuovoDaA.getText(); //Solo per le transazioni in uscita
+        if("Uscita".equals((String) tipoTransazione.getValue())){
+            destinatario = nuovoDaA.getText(); //Solo per le transazioni in uscita
             if (destinatario.isEmpty()){
                 showError("Il destinatario non può essere nullo per una transazione di uscita.");
                 return;  // Blocca l'inserimento se il mittente è vuoto
             }
-        } else if ("Entrata".equals((String) TipoTransazione.getValue())){
-            mittente= NuovoDaA.getText();
+        } else if ("Entrata".equals((String) tipoTransazione.getValue())){
+            mittente= nuovoDaA.getText();
             if (mittente.isEmpty()){
                 showError("Il mittente non può essere nullo per una transazione in entrata");
                 return;
             }
         }
         try {
-            float amount = Float.parseFloat(NuovoImporto.getText());
-            String valuta = (String)Valuta.getValue();
+            float amount = Float.parseFloat(nuovoImporto.getText());
+            String selectedValuta = (String)valuta.getValue();
 
-            float importo = convertToEuro(amount, valuta);
+            float importo = convertToEuro(amount, selectedValuta);
             // Crea l'oggetto Transazione utilizzando il costruttore completo
             Transazione nuovaTransazione = new Transazione(
                     null, // IDTransazione (generato dal database)
                     null, // CRO (generato dal database)
                     importo, // Importo
-                    java.sql.Date.valueOf(NuovaData.getValue()), // Data
+                    java.sql.Date.valueOf(nuovaData.getValue()), // Data
                     java.sql.Time.valueOf(java.time.LocalTime.now()), // Ora corrente
-                    NuovaCausale.getText(), // Causale
-                    (String) TipoTransazione.getValue(), // Tipo Transazione
+                    nuovaCausale.getText(), // Causale
+                    (String) tipoTransazione.getValue(), // Tipo Transazione
                     mittente, //mittente (solo per entrate)
                     destinatario, //destinatario (solo per uscite)
-                    (String) NumeroCarta.getValue(), // Numero Carta
-                    (String) NuovaCategoria.getValue() // Categoria
+                    (String) numeroCarta.getValue(), // Numero Carta
+                    (String) nuovaCategoria.getValue() // Categoria
             );
 
             // Inserimento nel database
@@ -121,12 +118,12 @@ public class AddTransactionController extends HomepageController{
     }
 
     private boolean validateInputs() {
-        if (TipoTransazione.getValue() == null ||
-                NuovoImporto.getText().isEmpty() ||
-                NuovaData.getValue() == null ||
-                NuovaCausale.getText().isEmpty() ||
-                NumeroCarta.getValue() == null ||
-                NuovaCategoria.getValue() == null) {
+        if (tipoTransazione.getValue() == null ||
+                nuovoImporto.getText().isEmpty() ||
+                nuovaData.getValue() == null ||
+                nuovaCausale.getText().isEmpty() ||
+                numeroCarta.getValue() == null ||
+                nuovaCategoria.getValue() == null) {
             showError("Tutti i campi obbligatori devono essere compilati.");
             return false;
         }
@@ -134,7 +131,7 @@ public class AddTransactionController extends HomepageController{
         //controlla che l'importo sia un numero validot
 
         try {
-        Float.parseFloat(NuovoImporto.getText());
+        Float.parseFloat(nuovoImporto.getText());
     } catch (NumberFormatException e) {
         showError("L'importo deve essere un numero valido.");
         return false;
--- Java/src/main/java/smu/Controller/FamigliaController.java
+++ Java/src/main/java/smu/Controller/FamigliaController.java
@@ -1,4 +1,50 @@
 package smu.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import javafx.util.Duration;
+import smu.DAO_Implementation.TransazioneDAOimp;
+import smu.DTO.Transazione;
+import smu.Sessione;
+import smu.DTO.Carta;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
+
 public class FamigliaController {
+    @FXML  private HBox comboBoxContainer; // Contenitore delle ComboBox e del pulsante
+    @FXML private Button selectFamilyButton;
+    @FXML private ComboBox<String> familyComboBox;
+    @FXML private ComboBox<String> portafoglioComboBox;
+
+
+
+
+    @FXML
+    private void initialize() throws SQLException {
+
+    }
+
+
+
+    @FXML
+    public void handleMouseClick(MouseEvent event) {
+        // Se si clicca al di fuori dell'HBox, chiudi le ComboBox
+        if (event.getTarget() != comboBoxContainer) {
+            familyComboBox.setVisible(false);
+            portafoglioComboBox.setVisible(false);
+        }
+    }
 }
+
