889ca3109bdf42976c8ceb605877537e32233672
miriam014 <mir.gaetano@studenti.unina.it>
2025-02-04 11:44:45 +0100

    ridimensionamento

--- Java/src/main/java/smu/Main.java
+++ Java/src/main/java/smu/Main.java
@@ -4,24 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-import smu.Controller.MenuController;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class Main extends Application {
     private static Stage primaryStage; // Riferimento allo stage principale
-    private static double lastWidth = 800; // Memorizza la larghezza dell'ultima finestra
-    private static double lastHeight = 600; // Memorizza l'altezza dell'ultima finestra
-    private static boolean FirstPageAfterLogin = true; // Flag per controllare la prima pagina dopo login
-    private static boolean isFullScreen = false; // Flag per verificare se la finestra è in modalità schermo intero
+    private static double currentWidth; // Memorizza la larghezza dell'ultima finestra
+    private static double currentHeight; // Memorizza l'altezza dell'ultima finestra
+    private static boolean firstPageAfterLogin = true; // Flag per controllare la prima pagina dopo login
 
     public static void main(String[] args) {
         launch(args); // Lancia l'applicazione JavaFX
@@ -44,54 +37,52 @@ public class Main extends Application {
 
     public static void setRoot(String fxml) throws IOException {
         URL fxmlLocation = Main.class.getResource("/interfaccia/" + fxml + ".fxml");
+        if (fxmlLocation == null) {
+            throw new IOException("File FXML non trovato");
+        }
         Parent root = FXMLLoader.load(fxmlLocation);
+
+        boolean wasFullScreen = primaryStage.isFullScreen(); // Controlla se la finestra era in fullscreen
+        primaryStage.setResizable(true);
 
         if(fxml.equals("login")) {
             primaryStage.setResizable(false);
             primaryStage.setScene(new Scene(root, 400, 350));
             primaryStage.centerOnScreen();
-            FirstPageAfterLogin = true; // la metto come vera così può essere modificata
-        } else if (fxml.equals("homepage") && FirstPageAfterLogin) {
-            primaryStage.setResizable(true);
+            firstPageAfterLogin = true; // la metto come vera così può essere modificata
+        } else if (fxml.equals("homepage") && firstPageAfterLogin) {
             primaryStage.setScene(new Scene(root, 800, 600));
-            lastWidth = 800; // Memorizza la larghezza
-            lastHeight = 600; // Memorizza l'altezza
-            FirstPageAfterLogin = false;    // la metto come falsa così non può essere più modificata
+            currentWidth = 800;
+            currentHeight = 600;
+            firstPageAfterLogin = false;    // la metto come falsa così non può essere più modificata
         } else {
-            primaryStage.setResizable(true);
-            primaryStage.setScene(new Scene(root, lastWidth, lastHeight));
+            primaryStage.setScene(new Scene(root, currentWidth, currentHeight));
+            primaryStage.setWidth(currentWidth);
+            primaryStage.setHeight(currentHeight);
+        }
+        // Nasconde il messaggio di uscita dallo schermo intero
+        primaryStage.setFullScreenExitHint("");
+        primaryStage.setFullScreen(wasFullScreen);
+        if (wasFullScreen) {
+            primaryStage.setFullScreen(true);
         }
 
-        // Gestisci modalità schermo intero
-        primaryStage.setFullScreenExitHint(""); // Nasconde il hint per uscire dal fullscreen
-        primaryStage.fullScreenProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                // Quando si entra in modalità schermo intero, memorizza la posizione e le dimensioni correnti
-                isFullScreen = true;
-                lastWidth = primaryStage.getWidth();
-                lastHeight = primaryStage.getHeight();
-            } else {
-                // Quando si esce dal fullscreen, ripristina le dimensioni
-                isFullScreen = false;
-                primaryStage.setWidth(lastWidth);
-                primaryStage.setHeight(lastHeight);
-            }
-        });
 
-        // Dopo aver impostato la scena, aggiorniamo le dimensioni solo se la finestra non è in schermo intero
-        primaryStage.widthProperty().addListener((observable, oldValue, newValue) -> {
-            if (!isFullScreen) {
-                // Aggiorna le dimensioni solo quando la finestra non è a schermo intero
-                lastWidth = newValue.doubleValue();
-            }
-        });
+        boolean inFullScreen = primaryStage.isFullScreen();
+        if (!inFullScreen) {
+            // Solo se non è in fullscreen, aggiorna le dimensioni
+            primaryStage.widthProperty().addListener((_, __, newValue) -> {
+                currentWidth = newValue.doubleValue();
+            });
 
-        primaryStage.heightProperty().addListener((observable, oldValue, newValue) -> {
-            if (!isFullScreen && (primaryStage.getHeight() != oldValue.doubleValue())) {
-                // Aggiorna le dimensioni solo quando la finestra non è a schermo intero
-                lastHeight = newValue.doubleValue();
-            }
-        });
+            primaryStage.heightProperty().addListener((_, __, newValue) -> {
+                currentHeight = newValue.doubleValue();
+            });
+        }
+
+        // Salva le dimensioni finali della finestra, evitando ridimensionamenti se fullscreen
+        currentHeight = primaryStage.getHeight();
+        currentWidth = primaryStage.getWidth();
 
         primaryStage.show();
     }
